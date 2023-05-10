5ee5274c1728f568b584860235be74b8ea4bde99
Dharmi Javiya <92503014+Dharmi7303@users.noreply.github.com>
2023-05-10 20:15:33 +0530

    Update and rename 20230414070218-initialdata.js to 20231683729839-initialdata.js

--- seeders/20231683729839-initialdata.js
+++ seeders/20231683729839-initialdata.js
@@ -6,31 +6,31 @@ const bcrypt = require("bcrypt");
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const saltRounds = 10;
-    const plainPasswords = ["vivekpassword", "pupilfirst", "testadmin"]; // Store the plain passwords in an array
+    const plainPasswords = ["dharmipassword", "pupilfirst", "admin"]; // Store the plain passwords in an array
     const hashedPasswords = await Promise.all(
       // Hash all the passwords using bcrypt
       plainPasswords.map((password) => bcrypt.hash(password, saltRounds))
     );
     const users = [
       {
-        name: "vivek",
-        email: "vivek@gmail.com",
+        name: "dharmi",
+        email: "dharmi@gmail.com",
         password: hashedPasswords[0],
         role: "admin",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
-        name: "wdcoach",
-        email: "wdcoach@gmail.com",
+        name: "wdpup",
+        email: "wdpup@gmail.com",
         password: hashedPasswords[1],
         role: "admin",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
-        name: "testadmin",
-        email: "testadmin@gmail.com",
+        name: "admin",
+        email: "admin@gmail.com",
         password: hashedPasswords[2],
         role: "admin",
         createdAt: new Date(),
@@ -44,5 +44,3 @@ module.exports = {
     await queryInterface.bulkDelete("Users", null, {});
   },
 };
-
- 
